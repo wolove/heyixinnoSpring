828c87cef08849fffd47093742686d7faf6ded1c
hyx <610302>
2017-07-06 17:39:49 +0800

    使用UTF-8编码properties

--- src/main/java/hyx/controller/RabbitMQController.java
+++ src/main/java/hyx/controller/RabbitMQController.java
@@ -10,12 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RabbitMQController {
 
-	@Autowired
-	RabbitTemplate template;
+    @Autowired
+    RabbitTemplate template;
 
-	@RequestMapping(value = "/rabbit/sent", method = RequestMethod.POST)
-	public String sent(@RequestParam String msg) {
-		template.convertAndSend("exchange", "routekey", msg);
-		return msg;
-	}
+    @RequestMapping(value = "/rabbit/sent", method = RequestMethod.POST)
+    public String sent(@RequestParam String msg) {
+        //这个exchange和routekey是要和declare的相同
+        template.convertAndSend("exchange", "routekey", msg);
+        return msg;
+    }
 }
